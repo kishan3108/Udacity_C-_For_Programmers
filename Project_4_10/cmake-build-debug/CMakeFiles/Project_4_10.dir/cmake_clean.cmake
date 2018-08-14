eb984876e18bfeb631f69cd02d59e5bde07efeba
CYNGN_Intern <cyngn_intern@news-MacBook-Pro.local>
2018-08-13 23:23:03 -0700

    Project_4_10

---
+++ Project_4_10/cmake-build-debug/CMakeFiles/Project_4_10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_4_10.dir/main.cpp.o"
+  "Project_4_10.pdb"
+  "Project_4_10"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_4_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
